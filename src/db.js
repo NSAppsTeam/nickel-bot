21dbffa6ea47ada500d959478f59286e3af5f0b6
Rob Rodriguez <roberto.rodriguez@nagrastar.com>
2017-03-09 23:17:23 +0000

    refactor: Switch to mongoose for db module

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
-const mongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 const env = require('./env');
 
 const URI = env.MONGODB_URI || 'mongodb://localhost:27017/review-bot';
@@ -9,7 +9,7 @@ module.exports = {
     uri = uri || URI;
 
     return new Promise((resolve, reject) => {
-      mongoClient.connect(uri, (err, db) => {
+      mongoose.connect(uri, (err, db) => {
         if (err) {
           return reject(err);
         }
