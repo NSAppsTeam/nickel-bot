d6280f8f7fff765ba2ff41d1338bbc12d929c9ab
Rob Rodriguez <roberto.rodriguez@nagrastar.com>
2017-03-09 23:18:46 +0000

    feat: Add schema for merge request

---
+++ src/merge-request-schema.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.schema;
+
+module.exports = new Schema({
+  merge_request: {
+    id: Number,
+    url: String,
+    target_branch: String,
+    source_branch: String,
+    created_at: String,
+    updated_at: String,
+    title: String,
+    description: String,
+    status: String,
+    work_in_progress: Boolean
+  },
+  last_commit: {
+    id: Number,
+    message: String,
+    timestamp: String,
+    url: String,
+    author: {
+      name: String,
+      email: String
+    }
+  },
+  project: {
+    name: String,
+    namespace: String
+  },
+  author: {
+    name: String,
+    username: String
+  },
+  assignee: {
+    claimed_on_slack: Boolean,
+    name: String,
+    username: String
+  }
+});
