4bab1cdf0074a95f6251d71662409bdaf0643640
Rob Rodriguez <roberto.rodriguez@nagrastar.com>
2017-03-14 17:54:54 +0000

    fix: Replace Mongoose promise support with another Q's

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const helmet = require('helmet');
 const winston = require('winston');
 const bodyParser = require('body-parser');
 const env = require('./src/env');
+const q = require('q');
+var mongoose = require('mongoose');
 
 var server = express();
 var router = require('./src/router');
 var PORT = env.PORT || 8000;
+
+// Built-in Promise support is deprecated
+mongoose.Promise = q.Promise;
 
 server.use(bodyParser.json());
 server.use(helmet());
